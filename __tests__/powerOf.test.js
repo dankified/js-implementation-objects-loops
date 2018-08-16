22830fa04ea4a75ca03cd45f6f828b7ca536ad56
Patricio de la Guardia <padlgt01@gmail.com>
2018-08-16 13:39:03 -0400

    Start version2

--- __tests__/powerOf.test.js
+++ __tests__/powerOf.test.js
@@ -1,4 +1,4 @@
-xdescribe('powerOfNames tests', () => {
+describe('powerOfNames tests', () => {
 	let powerOf = require('../exercises/powerOf');
 	it('should create arrays with numbers to the power of the second argument', () => {
 		expect(powerOf([1,2,3,4], 2)).toEqual([1, 4, 9, 16]);
