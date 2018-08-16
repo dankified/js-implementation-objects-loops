b1b1e19366d203fa575ce3c364ee1a0dbc87dacb
Patricio de la Guardia <padlgt01@gmail.com>
2018-08-16 13:32:49 -0400

    Start Version

--- exercises/iterateOverArrayAndCreatePeople.js
+++ exercises/iterateOverArrayAndCreatePeople.js
@@ -1,9 +1,3 @@
-function person(name, age, alive) {
-	return {name, age, alive};
-}
 
-function createPeople(arr) {
-	return arr.map((mem) => person(...mem));
-}
 
 module.exports = createPeople;
--- exercises/powerOf.js
+++ exercises/powerOf.js
@@ -1,6 +1,3 @@
-function powerOf(numberArray, power) {
-  return numberArray.map(num => num ** power);
-}
 
 
 module.exports = powerOf
